24ad359896aa9a630b5026022b93e053e4d54256
contactoeisenseguridad <contacto.eisensegurida@gmail.com>
2026-04-28 07:17:15 -0400

    Actualizar columnas filtros y paginacion admin alumnos

--- alumnos/admin.py
+++ alumnos/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from .models import Alumno, Inscripcion, Auditoria
-from .models import Aviso
+from .models import Alumno, Inscripcion, Auditoria, Aviso
+
 
 admin.site.register(Aviso)
+
 
 class InscripcionInline(admin.TabularInline):
     model = Inscripcion
@@ -11,9 +12,21 @@ class InscripcionInline(admin.TabularInline):
 
 @admin.register(Alumno)
 class AlumnoAdmin(admin.ModelAdmin):
-    list_display = ('nombres', 'apellidos', 'rut', 'correo')
-    search_fields = ('nombres', 'apellidos', 'rut')
+    list_display = ('nombres', 'apellidos', 'rut', 'grupo_actual', 'correo')
+    search_fields = ('nombres', 'apellidos', 'rut', 'correo')
+    list_filter = ('inscripcion__grupo',)
+    list_per_page = 100
+    ordering = ('apellidos', 'nombres')
     inlines = [InscripcionInline]
+
+    def grupo_actual(self, obj):
+        inscripcion = obj.inscripcion_set.order_by('-fecha_inicio').first()
+        if inscripcion:
+            return inscripcion.grupo
+        return '-'
+
+    grupo_actual.short_description = 'Grupo'
+    grupo_actual.admin_order_field = 'inscripcion__grupo'
 
     def has_change_permission(self, request, obj=None):
         if request.user.is_superuser:
@@ -41,7 +54,16 @@ class AlumnoAdmin(admin.ModelAdmin):
 @admin.register(Inscripcion)
 class InscripcionAdmin(admin.ModelAdmin):
     list_display = ('alumno', 'curso', 'grupo', 'fecha_inicio', 'estado')
-    search_fields = ('alumno__nombres', 'alumno__apellidos', 'curso', 'grupo')
+    search_fields = (
+        'alumno__nombres',
+        'alumno__apellidos',
+        'alumno__rut',
+        'curso',
+        'grupo',
+    )
+    list_filter = ('grupo', 'curso', 'estado', 'fecha_inicio')
+    list_per_page = 100
+    ordering = ('-fecha_inicio', 'grupo', 'alumno__apellidos')
 
     def has_change_permission(self, request, obj=None):
         if request.user.is_superuser:
@@ -71,6 +93,8 @@ class AuditoriaAdmin(admin.ModelAdmin):
     list_display = ('usuario', 'accion', 'modelo', 'objeto_id', 'fecha')
     search_fields = ('usuario', 'accion', 'modelo', 'descripcion')
     list_filter = ('accion', 'modelo', 'fecha')
+    list_per_page = 100
+    ordering = ('-fecha',)
 
     def has_add_permission(self, request):
         return False
@@ -83,6 +107,6 @@ class AuditoriaAdmin(admin.ModelAdmin):
 
 
 # 🔵 PERSONALIZACIÓN DEL ADMIN
-admin.site.site_header = "Sistema de Matrícula"
-admin.site.site_title = "Sistema de Matrícula"
+admin.site.site_header = "Sistema de Matrícula - OTEC Uno"
+admin.site.site_title = "Sistema de Matrícula - OTEC Uno"
 admin.site.index_title = "Panel de administración"
