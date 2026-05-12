69f57197dde1ab8dc8a7e3a76342b9f5dffd017f
contactoeisenseguridad <contacto.eisensegurida@gmail.com>
2026-05-11 23:21:40 -0400

    Asistencia 2

--- alumnos/urls.py
+++ alumnos/urls.py
@@ -7,17 +7,16 @@ from .views import (
     formulario_exportar_excel,
     exportar_excel_grupo,
     carga_masiva_alumnos,
+    portal_asistencia,  # 👈 1. Agrégala aquí
 )
 
 urlpatterns = [
     path('', inicio, name='inicio'),
-
     path('buscar/', buscar_alumno, name='buscar_alumno'),
     path('alumno/<int:alumno_id>/', detalle_alumno, name='detalle_alumno'),
     path('alumno/<int:alumno_id>/pdf/', generar_pdf, name='generar_pdf'),
-
     path('exportar-excel/formulario/', formulario_exportar_excel, name='formulario_exportar_excel'),
     path('exportar-excel/', exportar_excel_grupo, name='exportar_excel_grupo'),
-    path('asistencia/', views.portal_asistencia, name='portal_asistencia'),
+    path('asistencia/', portal_asistencia, name='portal_asistencia'), # 👈 2. Quita el "views."
     path('carga-masiva/', carga_masiva_alumnos, name='carga_masiva_alumnos'),
 ]
